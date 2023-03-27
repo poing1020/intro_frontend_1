05a8dcfd5c0c6c2dc2b10409e3b818468d12732f
unknown <d.cassmagiacolor@hotmail.com>
2023-03-27 16:05:38 -0600

    feat: api de ejemplo para npm

--- 1-node-npm/1.2-apis-express/index.js
+++ 1-node-npm/1.2-apis-express/index.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
   res.send("mi primer respuesta desde mi API en expressJS");
 });
 
+app.use(express.json());
+app.post('/',(req, res) => {
+  const body = req.body
+  console.log("recibi:", body);
+  res.status(201).send();
+
+});
+
 app.listen(port, () => {
   console.log(`servidor escuchando en el puerto ${port}`.rainbow)
 });
