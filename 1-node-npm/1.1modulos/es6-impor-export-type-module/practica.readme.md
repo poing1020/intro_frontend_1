49d20a7df41427f693220bfdce323e3510fb9f9b
poing1020 <d.cassmagiacolor@hotmail.com>
2023-04-08 13:36:06 -0600

    feat clean architecture example

---
+++ 1-node-npm/1.1modulos/es6-impor-export-type-module/practica.readme.md
@@ -0,0 +1,3 @@
+# descripcion 
+1.agregar la funcion squareRoot y exponertialte, para obtener la raiz y potencia de un numero
+2.Iportarlos desde main para mostrar los resultados 
--- 1-node-npm/1.2-apis-express/index.js
+++ 1-node-npm/1.2-apis-express/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send("mi primer respuesta desde mi API en expressJS");
+  res.send("mi segunda respuesta desde mi API en expressJS");
 });
 
 app.use(express.json());
